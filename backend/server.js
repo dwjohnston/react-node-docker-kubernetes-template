3dd31a60d4692a3428fa57d6a971cfd9946ca4c5
David Johnston <david@blacksheepcode.com>
2018-09-30 18:15:25 +1000

    Update to use environment variables

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,13 @@ const app = express();
 app.get("/random", (req, res) => {
   console.log("get random");
   res.setHeader("Content-Type", "application/json");
-  res.send(JSON.stringify({ value: Math.random() }));
+  res.send(
+    JSON.stringify({
+      value: Math.random(),
+      username: process.env.TEMPLATE_USERNAME,
+      password: process.env.TEMPLATE_PASSWORD
+    })
+  );
 });
 
 app.listen(3001, () => console.log("Example app listening on port 3001!"));
